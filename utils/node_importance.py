b11791a3350c3db2788623f6b93168dce9356e08
Janez Sedeljšak <janez.sedeljsak@gmail.com>
2024-06-11 12:35:41 +0200

    adjust personalized page rank function & exam progress

--- utils/node_importance.py
+++ utils/node_importance.py
@@ -21,19 +21,21 @@ def mu_corrected_clustering(graph):
     mu_corrected_cc = [cc / (degree if degree > 1 else 1) for cc, degree in zip(local_cc, degrees)]
     return mu_corrected_cc
 
-def personalized_pagerank(graph, node, damping_factor=0.85):
+def personalized_pagerank(graph, nodes, damping_factor=0.85):
     # Create a vector for the restart distribution
     restart_distribution = [0] * len(graph.vs)
-    restart_distribution[node] = 1
+    for node in nodes:
+        restart_distribution[node] = 1
 
     # Compute Personalized PageRank
     scores = graph.personalized_pagerank(damping=damping_factor, reset=restart_distribution)
     return scores
 
-def top_n_similar_nodes(graph, node, n=10):
-    scores = personalized_pagerank(graph, node)
+def top_n_similar_nodes(graph, nodes, n=10):
+    scores = personalized_pagerank(graph, nodes)
     sorted_indices = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)
-    sorted_indices.remove(node)
+    for node in nodes:
+        sorted_indices.remove(node)
     return sorted_indices[:n], scores
 
 def top_n_betweenness_nodes(G: Graph, n=10):
